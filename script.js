c8be9ffafdb5a93ddbadb25a7123c3aafbb81872
Scott Vorthmann <scott@vorthmann.org>
2025-06-26 07:52:58 -0700

    Initial implementation
    Creates collapsible "day" sections; no treatment of parallel tracks yet
    
    A GitHub Action downloads the `official-program.html` file, then builds
    for GitHub Pages.

---
+++ script.js
@@ -0,0 +1,52 @@
+
+fetch( './official-program.html' )
+  .then( response => response.text() )
+  .then( text => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString( text, 'text/html' );
+
+    const mainContent = doc.querySelector( 'main' );
+
+    // const sessions = mainContent.querySelectorAll( '.sched_block' );
+    // for ( const session of sessions ) {
+    //   const title = session.querySelector( 'p strong' );
+    //   if ( !!title ) {
+    //     console.log( title.textContent );
+    //   }
+    // }
+
+    const schedule = doc.querySelector( 'main .entry-content' );
+    const newDay = () => {
+      const today = document.createElement( 'div' );
+      today.classList.add( 'day' );
+      return today;
+    }
+
+    const intro = document.createElement( 'h4' );
+    intro.textContent = 'General Information';
+    let day = newDay();
+    intro.addEventListener( 'click', evt => evt.target.parentElement.classList.toggle( 'open' ) );
+    day.appendChild( intro );
+
+    const first = schedule.firstElementChild.nextElementSibling; // skip the style element
+    let child = first.nextElementSibling;
+    schedule.replaceChild( day, first );
+    day.appendChild( first );
+
+    while ( child ) {
+      const next = child.nextElementSibling;
+      if ( child.tagName === 'H4' ) {
+        day = newDay();
+        child.addEventListener( 'click', evt => evt.target.parentElement.classList.toggle( 'open' ) );
+        schedule.replaceChild( day, child );
+        day.appendChild( child );
+      } else if ( day ) {
+        day.appendChild( child );
+      }
+      child = next;
+    }
+
+
+    document.body.replaceChildren();
+    document.body.appendChild( mainContent );
+  } );
